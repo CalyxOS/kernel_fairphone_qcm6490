81fdc9827cce6f732eb6579693d42eae4fd4bd6c
Sultan Alsawaf <sultan@osomprivacy.com>
2023-05-12 13:48:52 -0700

    usb: dwc3: msm: Enforce usb_data_enabled by blocking role switches
    Block USB enumeration from the get-go by blocking role switches away from
    USB_ROLE_NONE when usb_data_enabled is false.
    
    Change-Id: I0eff78e56e4a3b64262f220a085cfec5910baf30
    Signed-off-by: Sultan Alsawaf <sultan@osomprivacy.com>

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -4080,9 +4080,6 @@ static int dwc3_msm_id_notifier(struct notifier_block *nb,
 	if (!edev || !mdwc)
 		return NOTIFY_DONE;
 
-	if (!mdwc->usb_data_enabled)
-		return NOTIFY_DONE;
-
 	dwc = platform_get_drvdata(mdwc->dwc3);
 
 	dbg_event(0xFF, "extcon idx", enb->idx);
@@ -4142,7 +4139,6 @@ static int dwc3_msm_vbus_notifier(struct notifier_block *nb,
 			dwc3_msm_gadget_vbus_draw(mdwc, 500);
 		else
 			dwc3_msm_gadget_vbus_draw(mdwc, 0);
-		return NOTIFY_DONE;
 	}
 
 	dwc = platform_get_drvdata(mdwc->dwc3);
@@ -4765,13 +4761,13 @@ static ssize_t usb_data_enabled_store(struct device *dev,
 				      const char *buf, size_t count)
 {
 	struct dwc3_msm *mdwc = dev_get_drvdata(dev);
+	bool enabled;
 
-	if (kstrtobool(buf, &mdwc->usb_data_enabled))
+	if (kstrtobool(buf, &enabled))
 		return -EINVAL;
 
-	if (!mdwc->usb_data_enabled) {
-		mdwc->vbus_active = false;
-		mdwc->id_state = DWC3_ID_FLOAT;
+	mdwc->usb_data_enabled = enabled;
+	if (!enabled) {
 		dwc3_ext_event_notify(mdwc);
 	}
 
