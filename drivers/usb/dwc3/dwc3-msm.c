5b912e5b9d62c72028910c8e921173b5b278ff49
Tommy Webb <tommy@calyxinstitute.org>
2025-01-23 15:43:51 +0000

    TESTING: Revert "Revert "usb: dwc3: Handle charging behavior when usb data is disabled""
    This reverts commit 4bd1b507e10ea853b8add0166bf4edf60ffd9c5a.
    
    Reason for revert: Charging was very slow (2%/hr).
    
    Change-Id: I4904a71347bac7bb8b7e2c7657ca529f2e21ab2b

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -4134,6 +4134,13 @@ static int dwc3_msm_vbus_notifier(struct notifier_block *nb,
 	if (!edev || !mdwc)
 		return NOTIFY_DONE;
 
+	if (!mdwc->usb_data_enabled) {
+		if (event)
+			dwc3_msm_gadget_vbus_draw(mdwc, 500);
+		else
+			dwc3_msm_gadget_vbus_draw(mdwc, 0);
+	}
+
 	dwc = platform_get_drvdata(mdwc->dwc3);
 
 	dbg_event(0xFF, "extcon idx", enb->idx);
@@ -5899,7 +5906,8 @@ static void dwc3_otg_sm_work(struct work_struct *w)
 			mdwc->drd_state = DRD_STATE_PERIPHERAL;
 			work = true;
 		} else {
-			dwc3_msm_gadget_vbus_draw(mdwc, 0);
+			if (mdwc->usb_data_enabled)
+				dwc3_msm_gadget_vbus_draw(mdwc, 0);
 			dev_dbg(mdwc->dev, "Cable disconnected\n");
 		}
 		break;
